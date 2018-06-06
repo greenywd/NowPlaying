872b662a77a6be2427fdec1688e70c03c3712179
Thomas Greenwood <greenydev@greenmail.net.au>
2018-06-06 21:41:29 +0930

    Added Mac Target

---
+++ NowPlayingMac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NowPlayingMac
+//
+//  Created by Thomas Greenwood on 6/6/18.
+//  Copyright © 2018 Thomas Greenwood. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
