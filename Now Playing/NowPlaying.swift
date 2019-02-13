27b3dc020e39a6055cd98dcffe99fa800395fc5a
greenywd <greenydev@greenmail.net.au>
2019-02-13 20:52:21 +1030

    Began implementing MVC

--- Now Playing/NowPlaying.swift
+++ Now Playing/NowPlaying.swift
@@ -7,3 +7,65 @@
 //
 
 import Foundation
+import MediaPlayer
+import UIKit
+
+class NowPlaying {
+    
+    // Struct created with static vars to store the contents of the current song - may be expanded in the future.
+    struct Song {
+        var title: String?
+        var albumTitle: String?
+        var artist: String?
+        var artwork: UIImage?
+    }
+    
+    init() {
+        MPMusicPlayerController.systemMusicPlayer.beginGeneratingPlaybackNotifications()
+    }
+    
+    func requestAuthorization() {
+        MPMediaLibrary.requestAuthorization() { status in
+            
+        }
+    }
+    
+    // Helper function to get the data from the Now Playing item and update the Song struct.
+    func getNowPlayingInfo() -> Song {
+        let systemMusicPlayer = MPMusicPlayerController.systemMusicPlayer.nowPlayingItem
+        
+        var np = Song()
+        np.title = systemMusicPlayer?.title ?? "Unknown Title"
+        np.artist = systemMusicPlayer?.artist ?? systemMusicPlayer?.albumArtist ?? "Unknown Artist"
+        np.albumTitle = systemMusicPlayer?.albumTitle ?? "Unknown Album"
+        np.artwork = systemMusicPlayer?.artwork?.image(at: (systemMusicPlayer?.artwork?.bounds.size)!) ?? nil
+        
+        return np
+    }
+    
+    func share() -> [Any] {
+        let np = self.getNowPlayingInfo()
+        
+        var toShare = [Any]()
+        let text = "Now Playing - " + (np.title ?? "Unknown Title") + " by " + (np.artist ?? "Unknown Artist")
+        
+        toShare.append(text)
+        
+        // If the user wants to share artwork, lets prepare it to be shared.
+        if UserDefaults.standard.bool(forKey: "artwork_enabled") {
+            if let artwork = np.artwork {
+                toShare.append(artwork)
+                // FIXME: Do we need to resize images?
+                // toShare.append(image.resizeImage(image: image, newWidth: 600))
+            }
+        }
+        
+        return toShare
+    }
+    
+    // https://gist.github.com/abhimuralidharan/3bcd28041f0bd81053c2f92f384ca693#file-settingsobserver-swift
+    func registerSettingsBundle(){
+        let appDefaults = [String:AnyObject]()
+        UserDefaults.standard.register(defaults: appDefaults)
+    }
+}
--- Now Playing/NowPlayingViewController.swift
+++ Now Playing/NowPlayingViewController.swift
@@ -12,15 +12,8 @@ import MediaPlayer
 class NowPlayingViewController: UIViewController {
 	
     // MARK: - Class Properties
+    let nowPlaying = NowPlaying()
     
-    // Struct created with static vars to store the contents of the current song - may be expanded in the future.
-	struct Song {
-		static var title: String = "Unknown Title"
-		static var albumTitle: String = "Unknown Album"
-		static var artist: String = "Unknown Artist"
-		static var artwork: UIImage?
-	}
-	
     // Instances used in created the background blur.
 	var backgroundArtworkImage: UIImage?
 	var blurEffectView: UIVisualEffectView?
@@ -31,12 +24,14 @@ class NowPlayingViewController: UIViewController {
 	@IBOutlet var titleLabel: UILabel!
 	
     // MARK: - Class Functions
-    
 	override func viewDidLoad() {
 		super.viewDidLoad()
-	
-		MPMusicPlayerController.systemMusicPlayer.beginGeneratingPlaybackNotifications()
 		
+        // TODO: Use this when user taps on a button/similar, not automagically upon startup
+        if MPMediaLibrary.authorizationStatus() != .authorized {
+            nowPlaying.requestAuthorization()
+        }
+        
 		// FIXME: App needs to be restarted after giving access to Media Library - maybe move the bulk of this into viewWillAppear?
 		// If we aren't authorized to use the Media Library, prompt for access.
 		if (MPMediaLibrary.authorizationStatus() != .authorized) {
@@ -53,8 +48,7 @@ class NowPlayingViewController: UIViewController {
 			NotificationCenter.default.addObserver(self, selector: #selector(self.updateLabels), name: .MPMusicPlayerControllerNowPlayingItemDidChange, object: nil)
 			NotificationCenter.default.addObserver(self, selector: #selector(self.share(_:)), name: .shareSong, object: nil)
 			
-			registerSettingsBundle()
-			getNowPlayingInfo()
+			nowPlaying.registerSettingsBundle()
 			updateLabels()
 		}
 	}
@@ -85,8 +79,6 @@ class NowPlayingViewController: UIViewController {
 		
 		if let blurView = self.view.viewWithTag(10) {
 			blurView.removeFromSuperview()
-		} else {
-			print("No need to remove blur view.")
 		}
 		
 		let blurEffect = UIBlurEffect(style: style)
@@ -96,25 +88,22 @@ class NowPlayingViewController: UIViewController {
 		
 		if let blurView = blurEffectView {
 			self.view.insertSubview(blurView, at: 0)
-		} else {
-			print("Error: Could not insert blurEffectView at layer 0.")
 		}
 	}
+    
     // TODO: Update function name.
 	// Updates the UI
 	@objc func updateLabels() {
-        // Before doing anything, let's grab the latest Now Playing data.
-        getNowPlayingInfo()
-        
+        let np = nowPlaying.getNowPlayingInfo()
 		let playbackState = MPMusicPlayerController.systemMusicPlayer.playbackState
 		
-		artistLabel.text = (playbackState == .stopped) ? "Start playing some music!" : Song.artist
-		titleLabel.text = (playbackState == .stopped) ? "" : Song.title
+		artistLabel.text = (playbackState == .stopped) ? "Start playing some music!" : np.artist
+		titleLabel.text = (playbackState == .stopped) ? "" : np.title
 		
 		if (playbackState == .stopped) {
 			artworkView.image = nil
 		} else {
-			if let artwork = Song.artwork {
+			if let artwork = np.artwork {
 				artworkView.image = artwork
 				backgroundArtworkImage = artwork
 				// TODO: center this?
@@ -124,60 +113,30 @@ class NowPlayingViewController: UIViewController {
 			}
 		}
 	}
-	
-    // Helper function to open the Settings app directly to our pane.
-	@objc func openApplicationSettings() {
-		if let url = URL(string:UIApplicationOpenSettingsURLString) {
-			if UIApplication.shared.canOpenURL(url) {
-				UIApplication.shared.open(url, options: [:], completionHandler: nil)
-			}
-		}
-	}
+    
+    @objc func openApplicationSettings() {
+        if let url = URL(string:UIApplicationOpenSettingsURLString) {
+            if UIApplication.shared.canOpenURL(url) {
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            }
+        }
+    }
     
     // TODO: Add "Share Album" option somewhere - ideally have both Song and Album options present to the user.
     // Let's share something!
 	@IBAction func share(_ sender: Any? = nil) {
+        sleep(1)
         // Move the user to the NowPlaying View - necessary for when activating via 3D Touch action.
 		tabBarController?.selectedIndex = 0
-		
-		var toShare = [Any]()
-		let text = "Now Playing - " + Song.title + " by " + Song.artist
-		
-		toShare.append(text)
-		
-        // If the user wants to share artwork, lets prepare it to be shared.
-		if UserDefaults.standard.bool(forKey: "artwork_enabled") {
-			if let artwork = Song.artwork {
-				toShare.append(artwork)
-				// FIXME: Do we need to resize images?
-				// toShare.append(image.resizeImage(image: image, newWidth: 600))
-			}
-		}
 		
 		// https://stackoverflow.com/a/35931947
 		
         // Prepare and present our share sheet.
-		let activityViewController = UIActivityViewController(activityItems: toShare, applicationActivities: nil)
+		let activityViewController = UIActivityViewController(activityItems: nowPlaying.share(), applicationActivities: nil)
 		activityViewController.popoverPresentationController?.sourceView = self.artworkView
 		activityViewController.excludedActivityTypes = [UIActivityType.airDrop, UIActivityType.saveToCameraRoll]
 		
 		self.present(activityViewController, animated: true, completion: nil)
-	}
-	
-	// https://gist.github.com/abhimuralidharan/3bcd28041f0bd81053c2f92f384ca693#file-settingsobserver-swift
-	func registerSettingsBundle(){
-		let appDefaults = [String:AnyObject]()
-		UserDefaults.standard.register(defaults: appDefaults)
-	}
-	
-    // Helper function to get the data from the Now Playing item and update the Song struct.
-	@objc func getNowPlayingInfo() {
-		let systemMusicPlayer = MPMusicPlayerController.systemMusicPlayer.nowPlayingItem
-		
-		Song.title = systemMusicPlayer?.title ?? "Unknown Title"
-		Song.artist = systemMusicPlayer?.artist ?? systemMusicPlayer?.albumArtist ?? "Unknown Artist"
-		Song.albumTitle = systemMusicPlayer?.albumTitle ?? "Unknown Album"
-		Song.artwork = systemMusicPlayer?.artwork?.image(at: (systemMusicPlayer?.artwork?.bounds.size)!) ?? nil
 	}
 	
 	// TODO: Update status bar colour on appearance change.
