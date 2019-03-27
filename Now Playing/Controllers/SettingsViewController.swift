f06810bcdca70ebcbb01ff86eaf2cbf82e5537f9
greenywd <greenydev@greenmail.net.au>
2019-03-27 19:06:58 +1030

    Refactored SettingsVC

--- Now Playing/Controllers/SettingsViewController.swift
+++ Now Playing/Controllers/SettingsViewController.swift
@@ -9,91 +9,84 @@
 import UIKit
 
 class SettingsViewController: UITableViewController, UITextFieldDelegate {
-
-	@IBOutlet var darkSwitch: UISwitch!
-	@IBOutlet var artworkSwitch: UISwitch!
-	@IBOutlet var confTextField: UITextField!
-	
+    
+    enum Theme: Int {
+        case light = 0
+        case dark = 1
+    }
+    
+    private var currentTheme: Theme {
+        return getCurrentTheme()
+    }
+    
+    private let userDefaults = UserDefaults.standard
+    
+    @IBOutlet var darkSwitch: UISwitch!
+    @IBAction func darkSwitchChanged(_ sender: UISwitch) {
+        userDefaults.set(sender.isOn, forKey: "dark_enabled")
+        updateAppearance()
+    }
+    
+    @IBOutlet var artworkSwitch: UISwitch!
+    @IBAction func artworkSwitchChanged(_ sender: UISwitch) {
+        userDefaults.set(sender.isOn, forKey: "artwork_enabled")
+    }
+    
+    @IBOutlet var confTextField: UITextField!
+    
     // MARK: - Class Functions
     
-	override func viewDidLoad() {
+    override func viewDidLoad() {
         super.viewDidLoad()
+
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        view.addGestureRecognizer(tap)
         
-		let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
-		view.addGestureRecognizer(tap)
-		defaultsChanged()
-		
-		NotificationCenter.default.addObserver(self, selector: #selector(self.defaultsChanged), name: UserDefaults.didChangeNotification, object: nil)
-		
-		darkSwitch.addTarget(self, action: #selector(updateUserDefaultsAppearanceDark), for: .valueChanged)
-		artworkSwitch.addTarget(self, action: #selector(updateUserDefaultsFunctionalityShareArtwork), for: .valueChanged)
-		
-		self.confTextField.delegate = self
+        self.confTextField.delegate = self
     }
-	
-	override func viewWillAppear(_ animated: Bool) {
-		setPrefStates()
-	}
-	
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        updateAppearance()
+        setPrefStates()
+    }
+    
     // MARK: - Class Functions (Custom)
     
-	func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-		UserDefaults.standard.set(self.confTextField.text, forKey: "share_text_conf")
-		
-		self.view.endEditing(true)
-		return false
-	}
-	
-    // Set dark mode on/off
-	@objc func updateUserDefaultsAppearanceDark(darkSwitch: UISwitch) {
-		UserDefaults.standard.set(darkSwitch.isOn, forKey: "dark_enabled")
-	}
-    
-	@objc func updateUserDefaultsFunctionalityShareArtwork(artworkSwitch: UISwitch) {
-		UserDefaults.standard.set(artworkSwitch.isOn, forKey: "artwork_enabled")
-	}
-	
-    // Update current UI if appearance is being change.
-	@objc func defaultsChanged() {
-		if (UserDefaults.standard.bool(forKey: "dark_enabled")) {
-			self.navigationController?.navigationBar.barStyle = .blackTranslucent
-			self.tabBarController?.tabBar.barStyle = .black
-		} else {
-			self.navigationController?.navigationBar.barStyle = .default
-			self.tabBarController?.tabBar.barStyle = .default
-		}
-	}
-	
-    // Set the UI elements to reflect the user's current preferences.
-	func setPrefStates() {
-		if (UserDefaults.standard.string(forKey: "dark_enabled") == "1") {
-			darkSwitch.setOn(true, animated: true)
-		} else {
-			darkSwitch.setOn(false, animated: true)
-		}
-		
-		if (UserDefaults.standard.string(forKey: "artwork_enabled") == "1") {
-			artworkSwitch.setOn(true, animated: true)
-		} else {
-			artworkSwitch.setOn(false, animated: true)
-		}
-        // TODO: Implement custom share text based on this key
-		confTextField.text = UserDefaults.standard.string(forKey: "share_text_conf")
-	}
-	
-	@objc func dismissKeyboard(){
-		view.endEditing(true)
-	}
-	
-	
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        userDefaults.set(self.confTextField.text, forKey: "share_text_conf")
+        
+        self.view.endEditing(true)
+        return false
     }
-    */
+    
+    // Set dark mode on/off
+    func updateAppearance() {
+        if currentTheme == .light {
+            self.navigationController?.navigationBar.barStyle = .default
+            self.tabBarController?.tabBar.barStyle = .default
+        } else {
+            self.navigationController?.navigationBar.barStyle = .black
+            self.tabBarController?.tabBar.barStyle = .black
+        }
+    }
 
+    // Set the UI elements to reflect the user's current preferences.
+    func setPrefStates() {
+        darkSwitch.isOn = userDefaults.bool(forKey: "dark_enabled")
+        artworkSwitch.isOn = userDefaults.bool(forKey: "artwork_enabled")
+
+        // TODO: Implement custom share text based on this key
+        confTextField.text = UserDefaults.standard.string(forKey: "share_text_conf")
+    }
+    
+    @objc func dismissKeyboard(){
+        view.endEditing(true)
+    }
+    
+    func getCurrentTheme () -> Theme {
+        let theme = userDefaults.bool(forKey: "dark_enabled")
+        return theme ? .dark : .light
+    }
 }
